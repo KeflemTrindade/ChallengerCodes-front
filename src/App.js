4707bf8e5decfb47425cc0a9142071e53787faf2
jose <jose.nascimento@abaco.com.br>
2020-10-06 20:01:44 -0400

    teste

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Challenger codes
+          Challenger codes Fatec
         </a>
       </header>
     </div>
